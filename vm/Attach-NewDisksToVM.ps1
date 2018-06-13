c4c35b4f5ea1333f127fd87dd1566358ab7a4109
Allen Kang <allenk@microsoft.com>
2018-06-13 15:03:06 +0800

    disk

---
+++ vm/Attach-NewDisksToVM.ps1
@@ -0,0 +1,34 @@
+# create new disks and attach to a VM.
+# managed disk https://docs.microsoft.com/en-us/azure/virtual-machines/windows/attach-disk-ps
+# unmanaged disk 
+import-module C:\kangxh\PowerShell\allenk-Module-Azure.psm1
+Add-AzureRMAccount-Allenk -myAzureEnv microsoft
+
+$rgName = 'az-rg-fta-bck'
+$vmName = 'azvmftabckdpm'
+$location = 'southeastasia' 
+$storageType = 'StandardLRS'
+$dataDiskSize = 128
+$dataDiskNum = 2
+
+#check current VM storage status, if it is managed disk, add managed disks, otherwize, add blob disks.
+$vm = Get-AzureRmVM -ResourceGroupName $rgName -Name $vmName
+if ($vm.StorageProfile.OsDisk.ManagedDisk -eq $null){
+    "unmanaged"
+    for ($i = 0; $i -lt $dataDiskNum; $i++){
+        $DataDiskVhdUri = $vm.StorageProfile.OsDisk.vhd.Uri.replace($vm.StorageProfile.OsDisk.Name, "$vmName-DataDisk-$i")
+        Add-AzureRmVMDataDisk -VM $vm -Name "$vmName-DataDisk-$i" -Caching 'ReadOnly' -DiskSizeInGB $dataDiskSize -Lun $i -VhdUri $DataDiskVhdUri -CreateOption Empty
+    }
+    Update-AzureRmVM -VM $vm -ResourceGroupName $rgName
+}
+else {
+    "managed"
+    for ($i = 0; $i -lt $dataDiskNum; $i++){
+        $diskConfig = New-AzureRmDiskConfig -SkuName  $storageType -Location $location -CreateOption Empty -DiskSizeGB $dataDiskSize
+        $dataDisk1 = New-AzureRmDisk -DiskName "$vmName-DataDisk-$i" -Disk $diskConfig -ResourceGroupName $rgName
+        
+        $vm = Get-AzureRmVM -Name $vmName -ResourceGroupName $rgName 
+        $vm = Add-AzureRmVMDataDisk -VM $vm -Name "$vmName-DataDisk-$i" -CreateOption Attach -ManagedDiskId $dataDisk1.Id -Lun $i
+    }
+    Update-AzureRmVM -VM $vm -ResourceGroupName $rgName
+}
