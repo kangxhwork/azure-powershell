05cb5a75bf829fc39c3843b73a54fe5b87877b6e
Allen Kang <allenk@microsoft.com>
2018-06-14 08:18:46 +0800

    gw

---
+++ iaas/Connect-AzureChinaWithVPNGW.ps1
@@ -0,0 +1,3 @@
+# VPN GW is the only connection why between China Azure and Global due to sepereate Subscription and AAD
+#  Network Peering and vNet-vNet connection not work in such scenario.
+# the script create VPN GW on both side and create connection.
