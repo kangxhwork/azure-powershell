9803b375276f0754d4f3e523bb856d1b181d115d
Allen Kang <allenk@microsoft.com>
2018-06-18 11:51:27 +0800

    gateway

--- iaas/Connect-AzureChinaVPNGW.ps1
+++ iaas/Connect-AzureChinaVPNGW.ps1
@@ -3,153 +3,79 @@
 # the script create VPN GW on both side and create connection.
 
 Import-Module C:\kangxh\PowerShell\allenk-Module-Azure.psm1
-$mcEnv=@{rg="mc-rg-fta-iaas"; localtion = "chinanorth";    vnet="mc-vnet-fta"; gateway="mc-vpn-fta"; pip="mc-pip-fta-vpngw"}
-$azEnv=@{rg="az-rg-fta-iaas"; localtion = "southeastasia"; vnet="az-vnet-fta"; gateway="az-vpn-fta"; pip="mc-pip-fta-vpngw"}
 
-#login to mooncake 
+$mcEnv=@{rg="mc-rg-fta-iaas"; localtion = "chinanorth";    vnet="mc-vnet-fta"; gateway="mc-vpn-fta"; pip="mc-pip-fta-vpngw"; localgateway = "mc-vpn-fta-localgw"; localgatewayconnection="mc-vpn-fta-localgw-connection"}
+$azEnv=@{rg="az-rg-fta-iaas"; localtion = "southeastasia"; vnet="az-vnet-fta"; gateway="az-vpn-fta"; pip="az-pip-fta-vpngw"; localgateway = "az-vpn-fta-localgw"; localgatewayconnection="az-vpn-fta-localgw-connection"}
+$SharedKey = 'AzureA1b2C3'
+#login to mooncake to create gateway
 Add-AzureRMAccount-Allenk -myAzureEnv mooncake
 
-$mcRg = Get-AzureRmResourceGroup -Name $mcEnv.rg -Location $mcEnv.localtion
-$mcVnet = Get-AzureRmVirtualNetwork -Name $mcEnv.vnet -ResourceGroupName $mcEnv.rg
-
-# Create PIP if it does not exist:
-$mcPip = Get-AzureRmPublicIpAddress -Name $mcEnv.pip -ResourceGroupName $mcenv.rg -ErrorAction Ignore
-if ($mcPip -eq $null)
-{
-    $mcPip  = New-AzureRmPublicIpAddress -Name $mcEnv.pip -ResourceGroupName $mcEnv.rg -Location $mcEnv.localtion -AllocationMethod Dynamic
-}
-
-# Create Gateway subnet if it has not been created.
-$mcGatewaySubnet = Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $mcVnet -Name "GatewaySubnet" -ErrorAction Ignore
-if ($mcGatewaySubnet -eq $null)
-{
-    Add-AzureRmVirtualNetworkSubnetConfig -Name $GWSubnetName -AddressPrefix $MCGWSubnetPrefix -VirtualNetwork $MCvNet | Set-AzureRmVirtualNetwork
-}
-
-# Create Gateway using the vNet and PIP created before
-$MCGW = Get-AzureRmVirtualNetworkGateway -Name $MCGWName -ResourceGroupName $MCRGName -ErrorAction Ignore
-if ($MCGW -eq $null)
-{
-    $MCGWSubnet = Get-AzureRmVirtualNetworkSubnetConfig -Name "GatewaySubnet" -VirtualNetwork $MCvNet
-    $MCGWIpConfig = New-AzureRmVirtualNetworkGatewayIpConfig -Name "MCGWIpConfig" -Subnet $MCGWSubnet -PublicIpAddress $MCPip
-    $MCGW = New-AzureRmVirtualNetworkGateway -Name $MCGWName -ResourceGroupName $MCRGName -Location $MCLocation -IpConfigurations $MCGWIpConfig -GatewayType Vpn -VpnType RouteBased -GatewaySku Basic -AsJob
-}
-
-azure
-======
-$connectionName = "AzureRunAsConnection"
-    
-    try
-    {
-        # Get the connection "AzureRunAsConnection "
-        $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
-
-        "Logging in to Azure..."
-        Add-AzureRmAccount `
-            -ServicePrincipal `
-            -TenantId $servicePrincipalConnection.TenantId `
-            -ApplicationId $servicePrincipalConnection.ApplicationId `
-            -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
-    }
-    catch {
-        if (!$servicePrincipalConnection)
-        {
-            $ErrorMessage = "Connection $connectionName not found."
-            throw $ErrorMessage
-        } else{
-            Write-Error -Message $_.Exception
-            throw $_.Exception
-        }
-    }
-
-    $RGName = "azure.allenk.lab"
-    $Location = "southeastasia"
-    $GWName = "az-vpn-gw"
-    $vNetName = "azure.allenk.lab"
-    $GWPIPName = "az-pip-vpn"
-    $GWSubnetPrefix = "10.0.254.0/24"
-    $GWSubnetName = "GatewaySubnet"
-
-    set-AzureRmContext -SubscriptionId 9c6835cb-2079-4f99-96f4-74029267e0df
-
-    $RG = Get-AzureRmResourceGroup -Name $RGName -Location $Location
-    $vNet = Get-AzureRmVirtualNetwork -Name $vNetName -ResourceGroupName $RGName
+$mcGateway = Get-AzureRmVirtualNetworkGateway -Name $mcEnv.gateway -ResourceGroupName $mcEnv.rg -ErrorAction Ignore
+if ($mcGateway -eq $null){
+    $mcVNET = Get-AzureRmVirtualNetwork -Name $mcEnv.vnet -ResourceGroupName $mcEnv.rg
 
     # Create PIP if it does not exist:
-    $Pip = Get-AzureRmPublicIpAddress -Name $GWPIPName -ResourceGroupName $RGName -ErrorAction Ignore
-    if ($Pip -eq $null)
-    {
-        $Pip  = New-AzureRmPublicIpAddress -Name $GWPIPName -ResourceGroupName $RGName -Location $Location -AllocationMethod Dynamic
+    $mcPip = Get-AzureRmPublicIpAddress -Name $mcEnv.pip -ResourceGroupName $mcEnv.rg -ErrorAction Ignore
+    if ($mcPip -eq $null)    {
+        $mcPip  = New-AzureRmPublicIpAddress -Name $mcEnv.pip -ResourceGroupName $mcEnv.rg -Location $mcEnv.localtion -AllocationMethod Dynamic
     }
 
     # Create Gateway subnet if it has not been created.
-    $GWSubnet = Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vNet -Name $GWSubnetName -ErrorAction Ignore
-    if ($GWSubnet -eq $null)
-    {
-        Add-AzureRmVirtualNetworkSubnetConfig -Name $GWSubnetName -AddressPrefix $GWSubnetPrefix -VirtualNetwork $vNet | Set-AzureRmVirtualNetwork
+    $mcGatewaySubnet = Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $mcVNET -Name "GatewaySubnet" -ErrorAction Ignore
+    if ($mcGatewaySubnet -eq $null){
+        Add-AzureRmVirtualNetworkSubnetConfig -Name "GatewaySubnet" -AddressPrefix $MCGWSubnetPrefix -VirtualNetwork $mcVNet | Set-AzureRmVirtualNetwork
     }
- 
+
     # Create Gateway using the vNet and PIP created before
-    $GW = Get-AzureRmVirtualNetworkGateway -Name $GWName -ResourceGroupName $RGName -ErrorAction Ignore
-    if ($GW -eq $null)
-    {
-        $GWSubnet = Get-AzureRmVirtualNetworkSubnetConfig -Name "GatewaySubnet" -VirtualNetwork $vNet
-        $GWIpConfig = New-AzureRmVirtualNetworkGatewayIpConfig -Name "GWIpConfig" -Subnet $GWSubnet -PublicIpAddress $Pip
-        $GW = New-AzureRmVirtualNetworkGateway -Name $GWName -ResourceGroupName $RGName -Location $Location -IpConfigurations $GWIpConfig -GatewayType Vpn -VpnType RouteBased -GatewaySku Basic -AsJob
+    $mcGatewaySubnet = Get-AzureRmVirtualNetworkSubnetConfig -Name "GatewaySubnet" -VirtualNetwork $mcVNET
+    $mcGatewayIpConfig = New-AzureRmVirtualNetworkGatewayIpConfig -Name "GatewayIpConfig" -Subnet $mcGatewaySubnet -PublicIpAddress $mcPip
+    $mcGateway = New-AzureRmVirtualNetworkGateway -Name $mcEnv.gateway -ResourceGroupName $mcEnv.rg -Location $mcenv.localtion -IpConfigurations $mcGatewayIpConfig -GatewayType Vpn -VpnType RouteBased -GatewaySku Basic -AsJob
+}else{
+    $mcVNET = Get-AzureRmVirtualNetwork -Name $mcEnv.vnet -ResourceGroupName $mcEnv.rg
+    $mcPip = Get-AzureRmPublicIpAddress -Name $mcEnv.pip -ResourceGroupName $mcEnv.rg 
+    $mcGatewaySubnet = Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $mcVNET -Name "GatewaySubnet"
+}
+
+#login to global azure to create gateway
+Add-AzureRMAccount-Allenk -myAzureEnv microsoft
+
+$azGateway = Get-AzureRmVirtualNetworkGateway -Name $azEnv.gateway -ResourceGroupName $azEnv.rg -ErrorAction Ignore
+if ($azGateway -eq $null){
+    $azVNET = Get-AzureRmVirtualNetwork -Name $azEnv.vnet -ResourceGroupName $azEnv.rg
+
+    # Create PIP if it does not exist:
+    $azPip = Get-AzureRmPublicIpAddress -Name $azEnv.pip -ResourceGroupName $azEnv.rg -ErrorAction Ignore
+    if ($azPip -eq $null)    {
+        $azPip  = New-AzureRmPublicIpAddress -Name $azEnv.pip -ResourceGroupName $azEnv.rg -Location $azEnv.localtion -AllocationMethod Dynamic
     }
 
+    # Create Gateway subnet if it has not been created.
+    $azGatewaySubnet = Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $azVNET -Name "GatewaySubnet" -ErrorAction Ignore
+    if ($azGatewaySubnet -eq $null){
+        Add-AzureRmVirtualNetworkSubnetConfig -Name "GatewaySubnet" -AddressPrefix $azGWSubnetPrefix -VirtualNetwork $azVNet | Set-AzureRmVirtualNetwork
+    }
 
-config
-    =====
+    # Create Gateway using the vNet and PIP created before
+    $azGatewaySubnet = Get-AzureRmVirtualNetworkSubnetConfig -Name "GatewaySubnet" -VirtualNetwork $azVNET
+    $azGatewayIpConfig = New-AzureRmVirtualNetworkGatewayIpConfig -Name "GatewayIpConfig" -Subnet $azGatewaySubnet -PublicIpAddress $azPip
+    $azGateway = New-AzureRmVirtualNetworkGateway -Name $azEnv.gateway -ResourceGroupName $azEnv.rg -Location $azenv.localtion -IpConfigurations $azGatewayIpConfig -GatewayType Vpn -VpnType RouteBased -GatewaySku Basic -AsJob
+}else{
+    $azVNET = Get-AzureRmVirtualNetwork -Name $azEnv.vnet -ResourceGroupName $azEnv.rg
+    $azPip = Get-AzureRmPublicIpAddress -Name $azEnv.pip -ResourceGroupName $azEnv.rg 
+    $azGatewaySubnet = Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $azVNET -Name "GatewaySubnet"
+}
 
-    # Setup connection environment
+# wait for above jobs to complete
+Get-Job | Wait-Job
 
-    $MCconnectionName = "MCRunAsConnection"   
-    $MCservicePrincipalConnection=Get-AutomationConnection -Name $MCconnectionName         
-    $MCEnv = Get-AzureRmEnvironment -Name AzureChinaCloud
+# Create local gateway and connection to bridge virtual networks
+    Add-AzureRMAccount-Allenk -myAzureEnv mooncake
 
-    $AzconnectionName = "AzureRunAsConnection"
-    $AzservicePrincipalConnection=Get-AutomationConnection -Name $AzconnectionName  
-
-
-# vNet-vNet cannot work. only IPset can work. 
-# Step 1: On Mooncake, get Network Gateway
-
-    "switch to Mooncake"
-    Add-AzureRmAccount -Environment $MCEnv -ServicePrincipal -TenantId $MCservicePrincipalConnection.TenantId -ApplicationId $MCservicePrincipalConnection.ApplicationId -CertificateThumbprint $MCservicePrincipalConnection.CertificateThumbprint 
-    Set-AzureRmContext -SubscriptionId c4013028-2728-46b8-acf1-e397840c4344
-    
-    # mooncake gateway
-    $MCGWPIP = Get-AzureRmPublicIpAddress -ResourceGroupName mooncake.allenk.lab -Name mc-pip-vpn
-    $MCGW = Get-AzureRmVirtualNetworkGateway -ResourceGroupName mooncake.allenk.lab -Name mc-vpn-gw -ErrorAction Ignore
-    if ($MCGW -eq $null) {exit}
-
-# Step 2: On Global azure, get Network Gateway
-
-    "switch to global azure"
-    Add-AzureRmAccount -ServicePrincipal -TenantId $AzservicePrincipalConnection.TenantId -ApplicationId $AzservicePrincipalConnection.ApplicationId -CertificateThumbprint $AzservicePrincipalConnection.CertificateThumbprint 
-    Set-AzureRmContext -SubscriptionId 9c6835cb-2079-4f99-96f4-74029267e0df
-
-    $AzGWPIP = Get-AzureRmPublicIpAddress -ResourceGroupName azure.allenk.lab -Name az-pip-vpn
-    $AzGW = Get-AzureRmVirtualNetworkGateway -ResourceGroupName azure.allenk.lab -Name az-vpn-gw -ErrorAction Ignore
-    if ($AzGW -eq $null) {exit}
-
-# Step 3: On Mooncake, create MCGW-AzGW connection
-
-    "switch to Mooncake"
-    Add-AzureRmAccount -Environment $MCEnv -ServicePrincipal -TenantId $MCservicePrincipalConnection.TenantId -ApplicationId $MCservicePrincipalConnection.ApplicationId -CertificateThumbprint $MCservicePrincipalConnection.CertificateThumbprint 
-    Set-AzureRmContext -SubscriptionId c4013028-2728-46b8-acf1-e397840c4344
-
-    $MCLocalGW = New-AzureRmLocalNetworkGateway -Name mc-local-gw-az -ResourceGroupName mooncake.allenk.lab -Location 'China North' -GatewayIpAddress $AzGWPIP.IpAddress  -AddressPrefix '10.0.0.0/16'
-    New-AzureRmVirtualNetworkGatewayConnection -Name mc-connection-az-gw -ResourceGroupName mooncake.allenk.lab -Location 'China North' -VirtualNetworkGateway1 $MCGW -LocalNetworkGateway2 $MCLocalGW -ConnectionType IPsec -RoutingWeight 10 -SharedKey 'AzureA1b2C3'
+    $mcLocalGateway = New-AzureRmLocalNetworkGateway -Name $mcEnv.localgateway -ResourceGroupName $mcEnv.rg -Location $mcEnv.location -GatewayIpAddress $azPip.IpAddress  -AddressPrefix $azVNET.AddressSpace
+    New-AzureRmVirtualNetworkGatewayConnection -Name $mcEnv.localgatewayconnection -ResourceGroupName $mcEnv.rg -Location $mcEnv.localtion -VirtualNetworkGateway1 $mcGateway -LocalNetworkGateway2 $mcLocalGateway -ConnectionType IPsec -RoutingWeight 10 -SharedKey $SharedKey
 
 # Step 4: On Global Azure, create AzGW-MCGW connection
+Add-AzureRMAccount-Allenk -myAzureEnv microsoft
 
-    "switch to global azure"
-    Add-AzureRmAccount -ServicePrincipal -TenantId $AzservicePrincipalConnection.TenantId -ApplicationId $AzservicePrincipalConnection.ApplicationId -CertificateThumbprint $AzservicePrincipalConnection.CertificateThumbprint 
-    Set-AzureRmContext -SubscriptionId 9c6835cb-2079-4f99-96f4-74029267e0df
-
-    $AzLocalGW = New-AzureRmLocalNetworkGateway -Name az-local-gw-mc -ResourceGroupName azure.allenk.lab -Location 'southeastasia' -GatewayIpAddress $MCGWPIP.IpAddress  -AddressPrefix '10.1.0.0/16'
-    New-AzureRmVirtualNetworkGatewayConnection -Name az-connection-mc-gw -ResourceGroupName azure.allenk.lab -Location 'southeastasia' -VirtualNetworkGateway1 $AzGW -LocalNetworkGateway2 $AzLocalGW -ConnectionType IPsec -RoutingWeight 10 -SharedKey 'AzureA1b2C3'
-
-    
+$azLocalGateway = New-AzureRmLocalNetworkGateway -Name $azEnv.localgateway -ResourceGroupName $azEnv.rg -Location $azEnv.location -GatewayIpAddress $mcPip.IpAddress  -AddressPrefix $mcVNET.AddressSpace
+New-AzureRmVirtualNetworkGatewayConnection -Name $azEnv.localgatewayconnection -ResourceGroupName $azEnv.rg -Location $azEnv.localtion -VirtualNetworkGateway1 $azGateway -LocalNetworkGateway2 $azLocalGateway -ConnectionType IPsec -RoutingWeight 10 -SharedKey $SharedKey
